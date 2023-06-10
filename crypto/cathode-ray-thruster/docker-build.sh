e95b6993c8ded09aa377be2bbd85e2e4fe787965
Antreas Pogiatzis <a.pogiatzis@iwoca.co.uk>
2023-06-10 01:53:32 +0100

    test file changes action

--- crypto/cathode-ray-thruster/docker-build.sh
+++
@@ -1,3 +0,0 @@
-IMAGE_NAME=cybermouflons/ccsc-2022-cathode-ray-thruster
-
-docker build ./setup -t $IMAGE_NAME
--- crypto/cathode-ray-thruster/docker-run.sh
+++
@@ -1,3 +0,0 @@
-IMAGE_NAME=cybermouflons/ccsc-2022-cathode-ray-thruster
-
-docker run -d -p 9000:9000 --restart=always $IMAGE_NAME
