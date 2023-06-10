aaa928eb70c490e3eef25fc298da2c34ae55f755
apogiatzis <apoyiatzis@outlook.com>
2023-06-10 05:29:30 +0100

    test file changes action

--- crypto/get_schwifty/docker-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t ccsc2022/get_schwifty setup/
--- crypto/get_schwifty/docker-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# --security-opt=seccomp:unconfined
-docker run -p 3137:3137 -d --restart=always ccsc2022/get_schwifty
