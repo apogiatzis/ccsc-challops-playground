c74f9a75d56406a14187481d647aa15a0876f79a
apogiatzis <apoyiatzis@outlook.com>
2023-06-10 05:00:56 +0100

    test file changes action

--- crypto/anatomy-park/docker-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-IMAGE_NAME=cybermouflons/ccsc-2022-anatomy-park
-
-docker build -t $IMAGE setup/
--- crypto/anatomy-park/docker-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IMAGE_NAME=cybermouflons/ccsc-2022-anatomy-park
-
-docker run -p 9002:13373 -d --restart=always $IMAGE_NAME
-
